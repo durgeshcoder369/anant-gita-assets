e16a079294e1e9f2549a86d64e57e90a25fff537
durgeshcoder369 <openmailon@gmail.com>
2026-05-26 23:14:00 +0530

    Refactor service worker for cleaner code

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,17 @@
 const CACHE_NAME = 'anant-gita-v1';
-const ASSETS = [
-  '/',
-  '/index.html'
-];
+const ASSETS = ['/', '/index.html'];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-    return cache.addAll(ASSETS);
-  }));
+  self.skipWaiting(); 
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
-
